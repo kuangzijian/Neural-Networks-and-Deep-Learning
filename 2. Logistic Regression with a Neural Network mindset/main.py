6da95b39e2fd95fd7465a512e7fb014d5100c9b3
Zijian Kuang <kuangzijian1@hotmail.com>
2019-10-31 16:30:58 -0600

    Added propagation and gradient descent functions

--- 2. Logistic Regression with a Neural Network mindset/main.py
+++ 2. Logistic Regression with a Neural Network mindset/main.py
@@ -4,6 +4,80 @@ import scipy
 from PIL import Image
 from scipy import ndimage
 from load_dataset import load_dataset
+from sigmoid import sigmoid
+from initialize_with_zeros import initialize_with_zeros
+from propagate import propagate
+from optimize import optimize
 
+print("============================== 2 - Overview of the Problem set =====================")
 # Loading the data (cat/non-cat)
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+
+# Example of a picture
+index = 25
+plt.imshow(train_set_x_orig[index])
+# plt.show()
+print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8")+"' picture.")
+
+
+### START CODE HERE ### (≈ 3 lines of code)
+m_train = train_set_x_orig.shape[0]
+m_test = test_set_x_orig.shape[0]
+num_px = train_set_x_orig.shape[1]
+### END CODE HERE ###
+
+print("Number of training examples: m_train = " + str(m_train))
+print("Number of testing examples: m_test = " + str(m_test))
+print("Height/Width of each image: num_px = " + str(num_px))
+print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print("train_set_x shape: " + str(train_set_x_orig.shape))
+print("train_set_y shape: " + str(train_set_y.shape))
+print("test_set_x shape: " + str(test_set_x_orig.shape))
+print("test_set_y shape: " + str(test_set_y.shape))
+
+
+# Reshape the training and test examples
+
+### START CODE HERE ### (≈ 2 lines of code)
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+### END CODE HERE ###
+
+print("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
+print("train_set_y shape: " + str(train_set_y.shape))
+print("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
+print("test_set_y shape: " + str(test_set_y.shape))
+print("sanity check after reshaping: " + str(train_set_x_flatten[0:5, 0]))
+
+train_set_x = train_set_x_flatten/255.
+test_set_x = test_set_x_flatten/255.
+
+print("======================== 4 - Building the parts of our algorithm ==========")
+print("======================= 4.1 - Helper functions ===========================")
+print("sigmoid([0, 2]) = " + str(sigmoid(np.array([0, 2]))))
+
+print("======================== 4.2 - Initializing parameters ===================")
+dim = 2
+w, b = initialize_with_zeros(dim)
+print("w = " + str(w))
+print("b = " + str(b))
+
+print("====================== 4.3 - Forward and Backward propagation ===========")
+
+w, b, X, Y = np.array([[1.],[2.]]), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
+grads, cost = propagate(w, b, X, Y)
+print("dw = " + str(grads["dw"]))
+print("db = " + str(grads["db"]))
+print("cost = " + str(cost))
+
+
+print("====================== 4.4 - Optimization ======================")
+params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
+
+print("w = " + str(params["w"]))
+print("b = " + str(params["b"]))
+print("dw = " + str(grads["dw"]))
+print("db = " + str(grads["db"]))
+
+
