6c8befb8f3a64a6d953a72d0724ac6b42a4fc140
Zijian Kuang <kuangzijian1@hotmail.com>
2019-11-01 10:19:07 -0600

    Minor fix

--- 2. Logistic Regression with a Neural Network mindset/model.py
+++ 2. Logistic Regression with a Neural Network mindset/model.py
@@ -21,8 +21,6 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0
     d -- dictionary containing information about the model.
     """
 
-    ### START CODE HERE ###
-
     # initialize parameters with zeros (≈ 1 line of code)
     w, b = initialize_with_zeros(X_train.shape[0])
 
@@ -36,8 +34,6 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0
     # Predict test/train set examples (≈ 2 lines of code)
     Y_prediction_test = predict(w, b, X_test)
     Y_prediction_train = predict(w, b, X_train)
-
-    ### END CODE HERE ###
 
     # Print train/test Errors
     print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
