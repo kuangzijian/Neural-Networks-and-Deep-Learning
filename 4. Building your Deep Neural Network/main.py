305bac4f2df61c06509e2ac5673d83d2a29c0460
Zijian Kuang <kuangzijian1@hotmail.com>
2019-11-24 16:43:37 -0700

    Completed  Building your deep neural network: Step by Step

--- 4. Building your Deep Neural Network/main.py
+++ 4. Building your Deep Neural Network/main.py
@@ -1,1 +1,105 @@
-import numpy
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from testCases_v4a import *
+from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
+from initialize_parameters import initialize_parameters
+from initialize_parameters_deep import initialize_parameters_deep
+from linear_forward import linear_forward
+from linear_activation_forward import linear_activation_forward
+from L_model_forward import L_model_forward
+from compute_cost import compute_cost
+from linear_backward import linear_backward
+from linear_activation_backward import linear_activation_backward
+from L_model_backward import L_model_backward
+from update_parameters import update_parameters
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+np.random.seed(1)
+
+print("================= 3 - Initialization =====================")
+print("# GRADED FUNCTION: initialize_parameters")
+
+print("================= 3.1 - 2-layer Neural Network ===========")
+parameters = initialize_parameters(3,2,1)
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+print("================ 3.2 - L-layer Neural Network ==========")
+parameters = initialize_parameters_deep([5,4,3])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+print("================ 4 - Forward propagation module =========")
+A, W, b = linear_forward_test_case()
+
+Z, linear_cache = linear_forward(A, W, b)
+print("Z = " + str(Z))
+
+print("=============== 4.2 - Linear-Activation Forward ========")
+A_prev, W, b = linear_activation_forward_test_case()
+
+A, linear_activation_cache = linear_activation_forward(A_prev, W, b, activation = "sigmoid")
+print("With sigmoid: A = " + str(A))
+
+A, linear_activation_cache = linear_activation_forward(A_prev, W, b, activation = "relu")
+print("With ReLU: A = " + str(A))
+
+print("# GRADED FUNCTION: L_model_forward")
+X, parameters = L_model_forward_test_case_2hidden()
+AL, caches = L_model_forward(X, parameters)
+print("AL = " + str(AL))
+print("Length of caches list = " + str(len(caches)))
+
+print("================ 5 - Cost function ======================")
+Y, AL = compute_cost_test_case()
+
+print("cost = " + str(compute_cost(AL, Y)))
+
+
+print("================ 6 - Backward propagation module =================")
+# Set up some test inputs
+dZ, linear_cache = linear_backward_test_case()
+
+dA_prev, dW, db = linear_backward(dZ, linear_cache)
+print ("dA_prev = "+ str(dA_prev))
+print ("dW = " + str(dW))
+print ("db = " + str(db))
+
+# GRADED FUNCTION: linear_activation_backward
+dAL, linear_activation_cache = linear_activation_backward_test_case()
+
+dA_prev, dW, db = linear_activation_backward(dAL, linear_activation_cache, activation = "sigmoid")
+print ("sigmoid:")
+print ("dA_prev = "+ str(dA_prev))
+print ("dW = " + str(dW))
+print ("db = " + str(db) + "\n")
+
+dA_prev, dW, db = linear_activation_backward(dAL, linear_activation_cache, activation = "relu")
+print ("relu:")
+print ("dA_prev = "+ str(dA_prev))
+print ("dW = " + str(dW))
+print ("db = " + str(db))
+
+
+print("===================== 6.3 - L-Model Backward =====================")
+AL, Y_assess, caches = L_model_backward_test_case()
+grads = L_model_backward(AL, Y_assess, caches)
+print_grads(grads)
+
+
+print("==================== 6.4 - Update Parameters =====================")
+parameters, grads = update_parameters_test_case()
+parameters = update_parameters(parameters, grads, 0.1)
+
+print ("W1 = "+ str(parameters["W1"]))
+print ("b1 = "+ str(parameters["b1"]))
+print ("W2 = "+ str(parameters["W2"]))
+print ("b2 = "+ str(parameters["b2"]))
